7d7919f8f810d425afb28eb7167dc09767ee8e6e
helgot <harrisonrobertholgate@gmail.com>
2024-12-28 17:57:24 +0000

    Version 1.1: Added first person and scoped sensitivity options.

--- CustomMouseSensitivity/main.cpp
+++ CustomMouseSensitivity/main.cpp
@@ -4,7 +4,7 @@
 
 #include "third_party\rdr2scripthook\inc\main.h"
 #include "script.h"
-
+#include "keyboard.h"
 
 
 BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved)
@@ -13,9 +13,11 @@ BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved)
 	{
 	case DLL_PROCESS_ATTACH:
 		scriptRegister(hInstance, ScriptMain);
+		keyboardHandlerRegister(OnKeyboardMessage);
 		break;
 	case DLL_PROCESS_DETACH:
 		scriptUnregister(hInstance);
+		keyboardHandlerUnregister(OnKeyboardMessage);
 		break;
 	}		
 	return TRUE;
--- CustomMouseSensitivity/script.cpp
+++ CustomMouseSensitivity/script.cpp
@@ -1,4 +1,5 @@
 #include "script.h"
+#include "keyboard.h"
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <iostream>
@@ -7,6 +8,7 @@
 #include <vector>
 #include <string>
 #include <ctime>
+#include <algorithm>
 
 using namespace std;
 
@@ -90,27 +92,81 @@ float GetPrivateProfileFloat(const std::string& section, const std::string& key,
     }
 }
 
+bool stringToBool(const std::string& str) {
+    // Convert the string to lowercase for case-insensitive comparison
+    std::string lowerStr = str;
+    std::transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(), ::tolower);
+
+    if (lowerStr == "true" || lowerStr == "1") {
+        return true;
+    } else if (lowerStr == "false" || lowerStr == "0") {
+        return false;
+    } else {
+        throw std::invalid_argument("Invalid string for boolean conversion: " + str);
+    }
+}
+bool string_to_bool(const std::string& str) {
+    // Convert the string to lowercase for case-insensitive comparison
+    std::string lowerStr = str;
+    std::transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(), ::tolower);
+
+    if (lowerStr == "true" || lowerStr == "1") {
+        return true;
+    }
+    else if (lowerStr == "false" || lowerStr == "0") {
+        return false;
+    }
+    else {
+        throw std::invalid_argument("Invalid string for boolean conversion: " + str);
+    }
+}
+
+float GetPrivateProfileBool(const std::string& section, const std::string& key, bool defaultValue, const std::string& fileName) {
+    char buffer[256];
+    // Retrieve the value as a string
+    GetPrivateProfileStringA(section.c_str(), key.c_str(), "", buffer, sizeof(buffer), fileName.c_str());
+
+    // Convert the string to a bool
+    try {
+        bool value;
+        return string_to_bool(buffer);
+    }
+    catch (const std::invalid_argument& e) {
+        // If conversion fails, return the default value
+        std::cerr << "Error: Invalid float value for key '" << key << "' in section '" << section << "'.\n";
+        return defaultValue;
+    }
+    catch (const std::out_of_range& e) {
+        // If the value is out of range, return the default value
+        std::cerr << "Error: Float value out of range for key '" << key << "' in section '" << section << "'.\n";
+        return defaultValue;
+    }
+}
+
+
 void main()
 {
     const std::string config_filename = "CustomMouseSensitivity.ini";
-    const float first_person_quake_sense = GetPrivateProfileFloat("Settings", "FirstPersonMouseSensitivity", 1.0f, GetCurrentDirectoryFilePath(config_filename));
-    const float third_person_quake_sense = GetPrivateProfileFloat("Settings", "ThirdPersonMouseSensitivity", 1.0f, GetCurrentDirectoryFilePath(config_filename));
-    
+    const float first_person_mouse_look_sensitivity = GetPrivateProfileFloat("FirstPerson", "FirstPersonMouseLookSensitivity", 1.0f, GetCurrentDirectoryFilePath(config_filename));
+    const float first_person_mouse_aim_sensitivity = GetPrivateProfileFloat("FirstPerson", "FirstPersonMouseAimSensitivity", 1.0f, GetCurrentDirectoryFilePath(config_filename));
+    const float third_person_mouse_look_sensitivity = GetPrivateProfileFloat("ThirdPerson", "ThirdPersonMouseLookSensitivity", 1.0f, GetCurrentDirectoryFilePath(config_filename));
+    const float third_person_mouse_aim_sensitivity = GetPrivateProfileFloat("ThirdPerson", "ThirdPersonMouseAimSensitivity", 1.0f, GetCurrentDirectoryFilePath(config_filename));
+    const bool fov_independent_rotation = GetPrivateProfileBool("FirstPerson", "FOVIndependentRotation", true, GetCurrentDirectoryFilePath(config_filename));
+    const float scoped_sensitivity = GetPrivateProfileFloat("Misc", "ScopedSensitivity", 1.0f, GetCurrentDirectoryFilePath(config_filename));
+
     int screen_x, screen_y;
     int line_height = 10;
-    GRAPHICS::GET_SCREEN_RESOLUTION(&screen_x, &screen_y);
     float fov = 0.0f;
-    float frame_time = 0.0f;
-    constexpr float target_frame_time = 0.0166667f;
+    constexpr float target_time_step = 0.0166667f;
     char buffer[64];
     bool is_first_person = false;
     bool is_in_scope = false;
     bool is_free_aiming = false;
-    float zoom_factor = 0.0f;
-    float stamina = 0.0f;
     int core_value = 0;
 
     const float base_first_person_fov = 55.0f;
+
+    bool debug_enabled = false;
 
     // Get base adddress of module:
     uintptr_t base_address = GetModuleBaseAddress("RDR2.exe");
@@ -123,10 +179,18 @@ void main()
     constexpr uintptr_t third_person_mouse_look_offset = 0x3973128; 
     constexpr uintptr_t third_person_mouse_aim_offset = 0x397312C; 
 
-    // Conversion constants:
     constexpr float core_empty_scale = 0.9881422924f;
-    const float first_person_scale_value = 0.00914375018f * first_person_quake_sense;
-    const float third_person_scale_value = 0.01117500011f * third_person_quake_sense;
+    constexpr float inv_core_empty_scale = 1.0f / core_empty_scale;
+    constexpr float first_person_scale_value = 0.00914375018f;
+    constexpr float third_person_scale_value = 0.01117500011f;
+    constexpr float default_scale_value = 0.01666667f;
+    //constexpr float scoped_sense_scale = 0.190525f;
+    constexpr float first_person_scoped_sense_scale = 0.087486349120136f;
+    constexpr float third_person_scoped_sense_scale = 0.087486349120136f;
+    constexpr float first_person_scoped_core_empty_scale = 1.823375f;
+
+    // 1.823375
+    // 0.104515 core empty scale sense.
  
     // Before main loop, patch slider values to all be 0.
     {
@@ -137,71 +201,118 @@ void main()
         PatchAddress(reinterpret_cast<void*>(base_address + third_person_mouse_aim_offset), reinterpret_cast<const void*>(&patch_value), sizeof(patch_value));
     }
 
-	while (true)
-	{		
-		// DEBUG Stuff:
-		//frame_time = GAMEPLAY::GET_FRAME_TIME();
-		//fov = CAM::GET_GAMEPLAY_CAM_FOV();
-        //zoom_factor = CAM::_GET_GAMEPLAY_CAM_ZOOM();
+    while (true)
+    {
+        // Acquire variables using ScriptHook:
+        GRAPHICS::GET_SCREEN_RESOLUTION(&screen_x, &screen_y);
+        fov = CAM::GET_GAMEPLAY_CAM_FOV();
         is_first_person = CAM::_0xD1BA66940E94C547();
-        //stamina = PLAYER::_0x0FF421E467373FCF(PLAYER::PLAYER_ID());
-        //is_in_scope = PLAYER::_0x04D7F33640662FA2(PLAYER::PLAYER_ID());
+        is_in_scope = PLAYER::_0x04D7F33640662FA2(PLAYER::PLAYER_ID());
         core_value = ATTRIBUTE::_0x36731AC041289BB1(PLAYER::PLAYER_PED_ID(), 1);
         is_free_aiming = PLAYER::IS_PLAYER_FREE_AIMING(PLAYER::PLAYER_ID());
 
-        // LOGGING:
-        /*
-		UI::SET_TEXT_SCALE(0.5f, 0.5f);
-		snprintf(buffer, sizeof(buffer), "frame_time: %.5f ms", frame_time * 1000.0f);
-		UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f);
-		snprintf(buffer, sizeof(buffer), "width: %d, height: %d", screen_x, screen_y);
-		UI::SET_TEXT_SCALE(0.5f, 0.5f);
-		UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 1 * 25.0f / screen_y);
-		UI::SET_TEXT_SCALE(0.5f, 0.5f);
-		snprintf(buffer, sizeof(buffer), "fov: %.4f", fov);
-		UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 2 * 25.0f / screen_y);
-		UI::SET_TEXT_SCALE(0.5f, 0.5f);
-		UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", is_first_person? "First person: True" : "First person: False"), 0.01f, 0.01f + 3 * 25.0f / screen_y);
-        UI::SET_TEXT_SCALE(0.5f, 0.5f);
-        snprintf(buffer, sizeof(buffer), "zoom factor: %.4f", zoom_factor);
-        UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 4 * 25.0f / screen_y);
-        UI::SET_TEXT_SCALE(0.5f, 0.5f);
-        UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", is_in_scope ? "scoped: True" : "scoped: False"), 0.01f, 0.01f + 5 * 25.0f / screen_y);
-        UI::SET_TEXT_SCALE(0.5f, 0.5f);
-        UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", is_free_aiming ? "is_free_aiming: True" : "is_free_aiming: False"), 0.01f, 0.01f + 6 * 25.0f / screen_y);
-        UI::SET_TEXT_SCALE(0.5f, 0.5f);
-        snprintf(buffer, sizeof(buffer), "stamina: %.4f", stamina);
-        UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 7 * 25.0f / screen_y);
-        UI::SET_TEXT_SCALE(0.5f, 0.5f);
-        snprintf(buffer, sizeof(buffer), "core: %d", core_value);
-        UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 8 * 25.0f / screen_y);
-        */
-        
-        // Apply patches:
-        if (is_first_person) // First-person:
+        if (debug_enabled)
         {
-            //WIP:
-            //float patch_value = first_person_scale_value;
-            //patch_value *= base_first_person_fov / fov;
-            //if (core_value == 0)
-            //{
-            //    patch_value *= core_empty_scale;
-            //}
-            ////patch_value *= frame_time / target_frame_time;
-            //PatchAddress(reinterpret_cast<void*>(base_address + first_person_scale_offset), reinterpret_cast<const void*>(&patch_value), sizeof(float));
-        }
-        else // Third-person:
-        {
-            float patch_value = third_person_scale_value;
-            if (is_free_aiming && core_value == 0)
-            {
-                patch_value *= core_empty_scale;
-            }
-            PatchAddress(reinterpret_cast<void*>(base_address + third_person_scale_offset), reinterpret_cast<const void*>(&patch_value), sizeof(float));
+            // LOGGING:
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            snprintf(buffer, sizeof(buffer), "FOV: %.4f", fov);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 0 * 25.0f / screen_y);
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", is_first_person ? "First person: True" : "First person: False"), 0.01f, 0.01f + 1 * 25.0f / screen_y);
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", is_in_scope ? "Scoped: True" : "Scoped: False"), 0.01f, 0.01f + 2 * 25.0f / screen_y);
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", is_free_aiming ? "FreeAiming: True" : "FreeAiming: False"), 0.01f, 0.01f +3 * 25.0f / screen_y);
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            snprintf(buffer, sizeof(buffer), "Stamina Core: %d", core_value);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 4 * 25.0f / screen_y);
+
+            // Input Values:
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            snprintf(buffer, sizeof(buffer), "ThirdPersonMouseLookSensitivity: %.4f", third_person_mouse_look_sensitivity);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 5 * 25.0f / screen_y);
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            snprintf(buffer, sizeof(buffer), "ThirdPersonMouseAimSensitivity: %.4f", third_person_mouse_aim_sensitivity);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 6 * 25.0f / screen_y);
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            snprintf(buffer, sizeof(buffer), "FirstPersonMouseLookSensitivity: %.4f", first_person_mouse_look_sensitivity);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 7 * 25.0f / screen_y);
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            snprintf(buffer, sizeof(buffer), "FirstPersonMouseAimSensitivity: %.4f", first_person_mouse_aim_sensitivity);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 8 * 25.0f / screen_y);
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", fov_independent_rotation ? "ScaleRotationWithFOV: True" : "ScaleRotationWithFOV: False"), 0.01f, 0.01f + 9 * 25.0f / screen_y);
+            UI::SET_TEXT_SCALE(0.5f, 0.5f);
+            snprintf(buffer, sizeof(buffer), "ScopedSensitivity: %.4f", scoped_sensitivity);
+            UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", buffer), 0.01f, 0.01f + 10 * 25.0f / screen_y);
         }
 
-		WAIT(0);
-	}
+        // Apply patches:
+        {
+            float patch_value = 0.0f;
+
+            if (is_in_scope) // In scope
+            {
+                if (is_first_person)
+                {
+                    patch_value = first_person_scoped_sense_scale * scoped_sensitivity;
+                }
+                else
+                {
+                    patch_value = third_person_scoped_sense_scale * scoped_sensitivity;
+                }
+
+                // Patch both values: 
+                PatchAddress(reinterpret_cast<void*>(base_address + first_person_scale_offset), reinterpret_cast<const void*>(&patch_value), sizeof(float));
+                PatchAddress(reinterpret_cast<void*>(base_address + third_person_scale_offset), reinterpret_cast<const void*>(&patch_value), sizeof(float));
+            }
+            else if (is_first_person) // First-person:
+            {
+                //WIP:
+                patch_value = first_person_scale_value;
+                if (is_free_aiming)
+                {
+                    patch_value *= first_person_mouse_aim_sensitivity;
+                }
+                else
+                {
+                    patch_value *= first_person_mouse_look_sensitivity;
+                }
+
+                if (fov_independent_rotation)
+                {
+                    patch_value *= base_first_person_fov / fov;
+                }
+
+                //patch_value *= time_step / target_time_step;
+                PatchAddress(reinterpret_cast<void*>(base_address + first_person_scale_offset), reinterpret_cast<const void*>(&patch_value), sizeof(float));
+            }
+            else // Third-person:
+            {
+
+                patch_value = third_person_scale_value;
+                if (is_free_aiming)
+                {
+                    patch_value *= third_person_mouse_aim_sensitivity;
+                }
+                else
+                {
+                    patch_value *= third_person_mouse_look_sensitivity;
+                }
+
+                if (is_free_aiming && core_value == 0)
+                {
+                    patch_value *= core_empty_scale;
+                }
+                PatchAddress(reinterpret_cast<void*>(base_address + third_person_scale_offset), reinterpret_cast<const void*>(&patch_value), sizeof(float));
+            }
+        }
+        if (IsKeyDown(VK_F9))
+        {
+            debug_enabled = !debug_enabled;
+        }
+        WAIT(0);
+    }
 }
 
 void ScriptMain()
